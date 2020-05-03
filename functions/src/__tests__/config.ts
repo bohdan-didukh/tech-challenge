ce6ea73bdc84d79343ecfe16236b6d3619a2a18c
Bohdan Didukh <bohdan.didukh@gmail.com>
2020-05-03 22:45:43 +0300

    :tada: Added functions.

---
+++ functions/src/__tests__/config.ts
@@ -0,0 +1,20 @@
+import * as TestFunctions from "firebase-functions-test";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCB4okRAmpAWX65zF9Rt7yPNI3-MBVXkgI",
+  authDomain: "basket-challenge.firebaseapp.com",
+  databaseURL: "https://basket-challenge.firebaseio.com",
+  projectId: "basket-challenge",
+  storageBucket: "basket-challenge.appspot.com",
+  messagingSenderId: "12868088025",
+  appId: "1:12868088025:web:6e9592fe7bb35bd1adcec8",
+};
+
+export const testFunctions = TestFunctions(
+  firebaseConfig,
+  "serviceAccountKey.json"
+);
+
+test("at least one test", () => {
+  expect(testFunctions).toBeDefined();
+});
---
+++ functions/src/baskets/getTotal.ts
@@ -0,0 +1,5 @@
+import * as functions from "firebase-functions";
+
+export const getTotal = functions.https.onCall((data, context) => {
+  return true;
+});
