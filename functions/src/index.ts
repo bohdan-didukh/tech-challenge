236987b3bc4c63c7cfead61025e3c3b27d7b5fc5
Bohdan Didukh <bohdan.didukh@gmail.com>
2020-05-08 17:44:24 +0300

    :fire: Removed functions.

--- functions/src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./baskets";
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
