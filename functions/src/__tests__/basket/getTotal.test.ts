eea1001821f8de15f8ffa96f106df056378f53a0
Bohdan Didukh <bohdan.didukh@gmail.com>
2020-05-12 02:23:03 +0300

    :white_check_mark: Added getTotal method with tests.

--- functions/src/__tests__/basket/getTotal.test.ts
+++ functions/src/__tests__/basket/getTotal.test.ts
@@ -5,6 +5,14 @@ import { testFunctions } from "../config";
 const wrappedGetTotal = testFunctions.wrap(myFunctions.getTotal);
 
 describe("calcTotal: empty", function () {
+  test("no user", async function () {
+    try {
+      const data = await wrappedGetTotal(null);
+      expect(data).toBeUndefined();
+    } catch (err) {
+      expect(err.code).toBe("invalid-argument");
+    }
+  });
   test("test empty basket", async function () {
     try {
       const data = await wrappedGetTotal(null, { auth: INITIAL_USER });
--- functions/src/basket/getTotal.ts
+++ functions/src/basket/getTotal.ts
@@ -1,10 +1,12 @@
 import * as functions from "firebase-functions";
-import { https } from "firebase-functions";
 import { calcTotal } from "./calcTotal";
 
 export const getTotal = functions.https.onCall((data, { auth }) => {
   if (!auth || !auth.uid) {
-    throw new https.HttpsError("invalid-argument", "{uid} is not valid");
+    throw new functions.https.HttpsError(
+      "invalid-argument",
+      "{uid} is not valid"
+    );
   }
 
   return calcTotal(auth.uid);
